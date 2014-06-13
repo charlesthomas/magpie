112ce320f351399a28e4d85ed88e1b71df4e7aef
looper <looperhacks@gmail.com>
2014-06-13 09:51:34 +0200

    Enable configuration from home directory
    This adds the possibility of defining multiple locations for the config-files. The given example first searches in ~/.magpie and if it doesn't find any config-files there, it searches in the default path. This enables configuration for each individual user and fixes the problem that magpie must be run as root if it was installed as root.

--- magpie/config/__init__.py
+++ magpie/config/__init__.py
@@ -1,9 +1,12 @@
 from os import path
 
 class ConfigPath(object):
+    def __init__(self):
+        self.config_paths = [path.join(path.expanduser('~'), '.magpie'), path.dirname(__file__)]
     def __getattr__(self, key):
-        return_path = path.join(path.dirname(__file__), key + '.cfg')
-        if not path.exists(return_path): return None
-        return return_path
+        for path in self.config_paths:
+            return_path = path.join(path, key + '.cfg')
+            if path.exists(return_path): return return_path
+        return None
 
 config_path = ConfigPath()
