a26c84a84fb57e43f934b89f5738339da23b924a
Charles Thomas <ch@rlesthom.as>
2014-07-11 21:20:55 -0400

    derp un reverting reversion

--- magpie/handler/base.py
+++ magpie/handler/base.py
@@ -1,46 +1,51 @@
-from os import listdir
-from os.path import isdir, join
-
 from base64 import b64encode, b64decode
+from os import listdir, path
+
 from tornado.web import RequestHandler
-from urllib import quote_plus, unquote_plus
 
 class BaseHandler(RequestHandler):
     def render(self, template, **kwargs):
-        kwargs.update(starred_notes=self.get_starred())
+        self.starred_notes = self.get_starred()
+        kwargs.update(starred_notes=self.starred_notes)
+
         notebook_name = kwargs.get('notebook_name', None)
         if notebook_name is not None:
             kwargs['notes'] = self._notes_list(notebook_name)
         else:
             kwargs['notebook_name'] = ''
             kwargs['notes'] = []
-        if self.settings.repo is not None and isdir(self.settings.repo) and \
-        not kwargs.get('hide_notebooks', False):
-            kwargs['notebooks'] = sorted(listdir(self.settings.repo))
-        else:
-            kwargs['notebooks'] = []
-        cleanbooks = []
-        cleannotes = []
-        for s in kwargs['notebooks']:
-            if s != '':
-                cleanbooks.append(self._decode_notename(s))
-        for s in kwargs['notes']:
-            if s != '':
-                cleannotes.append(self._decode_notename(s))
-        kwargs['notebooks'] = cleanbooks
-        kwargs['notes']     = cleannotes
+
+        kwargs['notebooks'] = self._notebooks_list(kwargs.get('hide_notebooks',
+                                                              False))
         super(BaseHandler, self).render(template, **kwargs)
 
-    def _notes_list(self, notebook_name):
-        path = join(self.settings.repo, self._encode_notename(notebook_name))
-        notes = sorted([n for n in listdir(path) if (u'%s/%s' % (notebook_name, 
-                        self._decode_notename(n).decode('utf8').replace(' ', '+'))) in 
-                        self.get_starred()])
-        notes += sorted([n for n in listdir(path) if not n.startswith('.') and \
-                         n not in notes])
-        return notes
+    def _notebooks_list(self, hide_notebooks=False):
+        if self.settings.repo is None or not path.isdir(self.settings.repo) or \
+        hide_notebooks:
+            return []
 
-    def _highlight(self, text, highlight):
+        return sorted([self.decode_name(nb) for nb in \
+                       listdir(self.settings.repo) if nb != ''])
+
+    def _notes_list(self, notebook_name):
+        notes_path = path.join(self.settings.repo,
+                               self.encode_name(notebook_name))
+
+        all_notes = [self.decode_name(n) for n in listdir(notes_path) if not \
+                     n.startswith('.')]
+        starred = []
+        unstarred = []
+
+        while all_notes:
+            note = all_notes.pop()
+            if u'%s/%s' % (notebook_name, note) in self.starred_notes:
+                starred.append(note)
+            else:
+                unstarred.append(note)
+
+        return sorted(starred) + sorted(unstarred)
+
+    def highlight(self, text, highlight):
         return text.replace(highlight,
                             "<font style=background-color:yellow>%s</font>" % \
                             highlight)
@@ -52,14 +57,14 @@ class BaseHandler(RequestHandler):
 
         return self.get_cookie('session', '') == self.settings.session
 
-    def _encode_notename(self, name):
-        return quote_plus(name.replace('+', ' ').encode('utf8'), ' -_')
+    def encode_name(self, name):
+        return name.replace('+', ' ').encode('utf8')
 
-    def _decode_notename(self, name):
-        return unquote_plus(name.encode('utf8')).replace('+', ' ')
+    def decode_name(self, name):
+        return name.encode('utf8').replace('+', ' ')
 
     def get_starred(self):
         starred_list = self.get_cookie('starred_notes')
         if starred_list is None:
             return []
-        return b64decode(starred_list).decode('utf8').split(',')
+        return b64decode(starred_list).decode('utf8').replace('+', ' ').split(',')
