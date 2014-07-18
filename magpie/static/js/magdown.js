4cb6ca89b5664c9c80dba8be62d46311273ebaf5
Tony O'Dell <tony.odell@live.com>
2014-07-18 10:53:53 -0700

    changed external libs to submodules

--- magpie/static/js/magdown.js
+++ magpie/static/js/magdown.js
@@ -1,43 +1,46 @@
-$("*[contenteditable='true']").hallo({
-  plugins: {
-    'halloformat': {},
-    'halloblock': {},
-    'hallojustify': {},
-    'hallolists': {},
-    //'hallolink': {},
-    'halloreundo': {}
-  },
-  editable: true,
+$(document).ready(function(){
+  $("*[contenteditable='true']").hallo({
+    plugins: {
+      'halloformat': {},
+      'halloblock': {},
+      'hallojustify': {},
+      'hallolists': {},
+      //'hallolink': {},
+      'halloreundo': {}
+    },
+    editable: true,
+    toolbar: "halloToolbarFixed"
+  });
+
+  var markdownize = function(content) {
+    var html = content.split("\n").map($.trim).filter(function(line) { 
+      return line != "";
+    }).join("\n");
+    return toMarkdown(html);
+  };
+  var converter = new Showdown.converter();
+  var htmlize = function(content) {
+    return converter.makeHtml(content);
+  };
+
+  var showSource = function(content) {
+    var markdown = markdownize(content);
+    if($("textarea").get(0).value == markdown){
+      return;
+    }
+    $("textarea").get(0).value = markdown;
+  };
+
+  var updateHtml = function(content) {
+    if (markdownize($("*[contenteditable='true']").html()) == content) {
+      return;
+    }
+    var html = htmlize(content);
+    $("*[contenteditable='true']").html(html); 
+  };
+
+  $("*[contenteditable='true']").bind('hallomodified', function(event, data) {
+    showSource(data.content);
+  });
+  updateHtml($("textarea").html());
 });
-
-var markdownize = function(content) {
-  var html = content.split("\n").map($.trim).filter(function(line) { 
-    return line != "";
-  }).join("\n");
-  return toMarkdown(html);
-};
-var converter = new Showdown.converter();
-var htmlize = function(content) {
-  return converter.makeHtml(content);
-};
-
-var showSource = function(content) {
-  var markdown = markdownize(content);
-  if($("textarea").get(0).value == markdown){
-    return;
-  }
-  $("textarea").get(0).value = markdown;
-};
-
-var updateHtml = function(content) {
-  if (markdownize($("*[contenteditable='true']").html()) == content) {
-    return;
-  }
-  var html = htmlize(content);
-  $("*[contenteditable='true']").html(html); 
-};
-
-$("*[contenteditable='true']").bind('hallomodified', function(event, data) {
-  showSource(data.content);
-});
-updateHtml($("textarea").html());
