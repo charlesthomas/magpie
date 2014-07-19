daa025d48f9d6024c76c8ce6b66e390cafee66b9
Charles Thomas <ch@rlesthom.as>
2014-07-19 16:13:51 -0400

    making some small test updates, first inclusion of Dulwich in reqs

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -39,3 +39,12 @@ class Test(BaseTest):
         note_name = u'übernöte'
         self.touch(note_name)
         self.get(self.notebook_url + quote(note_name.encode('utf8')))
+
+    def test_create_note(self):
+        note_name = 'test_note'
+        res = self.post(self.notebook_url + note_name,
+                        allow_errors=True,
+                        save=True,
+                        note='this is a test note')
+        test_path = path.join(self.notebook_path, note_name)
+        self.assertTrue(path.exists(test_path))
